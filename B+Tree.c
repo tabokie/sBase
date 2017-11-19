4e94ab70eafddefffb0ae53953bb65c17f0b5d6b
tabokie <james_199@126.com>
2017-11-19 19:24:22 +0800

    Complete B+tree, fix bug, some remains.

--- B+Tree.c
+++ B+Tree.c
@@ -5,7 +5,9 @@
 #define New(typeName,size)			(typeName*)malloc(sizeof(typeName)*(size))
 #define Initial(array,val,size)		({int i;for(i=0;i<size;i++)array[i]=val;})
 #define PutInts(array,size)			({printf(#array);printf(": ");int i;for(i=0;i<size;i++)printf("%d ",array[i]);printf("\n");})
-#define For(i,a,b)					for(i=a;(a<=b)?(b-a):(a-b)>0;i+=(a<=b)?1:-1)
+#define For(i,a,b)					for(i=a;(a<=b)?(b-i):(i-b)>0;i+=(a<=b)?1:-1)
+#define Log()						({printf("%s",__func__);printf("\n");})
+#define BP(note)					({printf(#note);printf(", In function %s",__func__);printf("\n");})
 
 /*
 about B+tree
@@ -23,11 +25,12 @@ about B+tree
 
 
 #define M 				(3)
+#define Inf 			(1e3)
 typedef int index_type;
 typedef int* addr_type;
 
 
-int findInterval(index_type* index, int size){
+int findInterval(index_type* index, index_type idx, int size){
 	int lo=-1,hi=size,mid;
 	while(hi-lo>=2){
 		mid=lo+(hi-lo)/2;
@@ -54,18 +57,21 @@ typedef struct LeafPage{
 }* leaf_page;
 
 typedef struct IndexTree{
+	int size;//recognize code
 	index_page root;
-	unsigned int height;
+	int height;
 	leaf_page l_leaf;
 	int l_size;
 }* index_tree;
 
 leaf_page newLeafPage(void){
+	Log();
 	leaf_page newPage=New(struct LeafPage,1);
+
 	newPage->size=0;
-	newPage->key=New(index_type,M);
+	newPage->key=New(index_type,M+1);
 	Initial(newPage->key,(index_type)0,M+1);
-	newPage->addr=New(addr_type,M);
+	newPage->addr=New(addr_type,M+1);
 	Initial(newPage->addr,(addr_type)NULL,M+1);
 	newPage->next=NULL;
 	newPage->pre=NULL;
@@ -73,6 +79,7 @@ leaf_page newLeafPage(void){
 }
 
 index_page newIndexPage(void){
+	Log();
 	index_page newPage=New(struct IndexPage,1);
 	newPage->size=0;
 	newPage->pointer=New(index_page,M+2);
@@ -83,6 +90,7 @@ index_page newIndexPage(void){
 }
 
 int freeLeafPage(leaf_page l){
+	Log();
 	free(l->key);
 	free(l->addr);
 	free(l);
@@ -90,41 +98,52 @@ int freeLeafPage(leaf_page l){
 }
 
 int freeIndexPage(index_page index){
+	Log();
 	free(index->key);
-	free(index->addr);
+	free(index->pointer);
 	free(index);
 	return 1;
 }
 
 index_page firstIndexPage(void){
+	Log();
 	index_page first=newIndexPage();
 	int i;
 
 	leaf_page cur=newLeafPage();
 	first->pointer[0]=cur;//[0]'s pre is NULL
+	cur->father=first;
 	For(i,1,M+1){
-		first->pointer[i]=newLeafPage();
-		cur->next=first->pointer[i];
-		first->pointer[i]->pre=cur;
+		cur->next=newLeafPage();
+		cur->next->pre=cur;
 		cur=cur->next;
+		first->pointer[i]=cur;
+		cur->father=first;
 	}
-	//cur->next=first->pointer[0];
+	cur->next=first->pointer[0];
 	cur->next=NULL;//symmetricity
 	return first;
 }
 
 index_tree initialIndexTree(void){
+	Log();
 	index_tree newTree=New(struct IndexTree,1);
+	newTree->size=Inf;
 	newTree->root=firstIndexPage();
+	newTree->root->father=newTree;
+
 	newTree->height=1;
 	newTree->l_leaf=newTree->root->pointer[0];
+
 	newTree->l_size=0;
 	return newTree;
 }
 
 int changeIndex(index_page index, index_type init, index_type new){
+	Log();
+	if(index==NULL||index->size==Inf)return 1;
 	int i;
-	For(i,0,M)if(index->key[i]==init)break;
+	For(i,0,index->size)if(index->key[i]==init)break;
 	if(i==M)return 0;
 	else index->key[i]=new;
 	if(i==0)return changeIndex(index->father, init, new);
@@ -132,38 +151,41 @@ int changeIndex(index_page index, index_type init, index_type new){
 }
 
 int insertToleaf(leaf_page l, index_type idx, addr_type addr){
+	Log();
 	if(l->size>=M)return 0;
 	int i;
-	int lo=findInterval(l->key,i->size);
+	int lo=findInterval(l->key,idx,l->size);
 	if(lo==-1&&!changeIndex(l->father,l->key[0],idx))Error(Fail to change upper index!);
-	For(i,l->size,hi){
+	For(i,l->size,lo+1){
 		l->key[i]=l->key[i-1];
 		l->addr[i]=l->addr[i-1];
 	}
-	l->key[hi]=idx;
-	l->addr[hi]=addr;
+	l->key[lo+1]=idx;
+	l->addr[lo+1]=addr;
 
 	l->size++;
 	return 1;
 }
 int insertToleaf_withBuffer(leaf_page l, index_type idx, addr_type addr){
+	Log();
 	if(l->size!=M)return 0;
 	int i;
 	//l->size=M
-	int lo=findInterval(l->key,i->size);
+	int lo=findInterval(l->key,idx,l->size);
 	if(lo==-1&&!changeIndex(l->father,l->key[0],idx))Error(Fail to change upper index!);
-	For(i,l->size,hi){
+	For(i,l->size,lo+1){
 		l->key[i]=l->key[i-1];
 		l->addr[i]=l->addr[i-1];
 	}
-	l->key[hi]=idx;
-	l->addr[hi]=addr;
+	l->key[lo+1]=idx;
+	l->addr[lo+1]=addr;
 
 	l->size++;
 	return 1;
 }
 
 int insertToIndex_withBuffer(index_page index, int p, index_type new){
+	Log();
 	int i;
 	For(i,index->size,p){
 		index->key[i]=index->key[i-1];
@@ -174,12 +196,14 @@ int insertToIndex_withBuffer(index_page index, int p, index_type new){
 
 //deserted
 int replacePointer(index_page index, int p, leaf_page new){
+	Log();
 	if(index->size<p)return 0;
 	index->pointer[p]=new;
 	return 1;
 }
 
 int insertPointer_withBuffer(index_page index,int p,leaf_page new){//insert to idx(p)
+	Log();
 	int i;
 	For(i,index->size+1,p){
 		index->pointer[i]=index->pointer[i-1];
@@ -189,34 +213,53 @@ int insertPointer_withBuffer(index_page index,int p,leaf_page new){//insert to i
 }
 
 leaf_page newLeafPage_loaded(leaf_page l, int init, int size){
+	Log();
 	leaf_page new=newLeafPage();
+	int i;
 	For(i,init,init+size){
 		new->key[i-init]=l->key[i];
 		new->addr[i-init]=l->addr[i];
 	}
+	new->addr[size]=l->addr[init+size];
 	new->size=size;
 	//
 	new->father=l->father;
 	return new;
 }
 
+index_page newIndexPage_loaded(index_page index, int init, int size){
+	Log();
+	index_page new=newIndexPage();
+	int i;
+	For(i,init,init+size){
+		new->key[i-init]=index->key[i];
+		new->pointer[i-init]=index->pointer[i];
+	}
+	new->pointer[size]=index->pointer[init+size];
+	new->size=size;
+	//
+	new->father=index->father;
+	return new;
+}
+
 int splitLeaf(leaf_page l){//related to father
+	Log();
 	if(l->size<=M)return 0;
 	if(l->father->size>=M+1)return 0;
 	//safe to load to father
-	int i,pivot;
-	For(i,0,l->father->size)if(l->father->key[i]==l->key[0])break;
-	if(i==l->father->size)return 0;
+	int pivot;
+	For(pivot,0,l->father->size)if(l->father->key[pivot]==l->key[0])break;
+	if(pivot==l->father->size)return 0;
 	//determine the new index to insert
 	index_type newIdx=l->key[(M+1)/2];
 	int size1=(M+1)/2;
 	int size2=(M+1-(M+1)/2+1);
 	//insert pointers to pivot+1 and pivot+2
 	//right part to replace
-	leaf_page right=newLeafPage_loaded(l,0,size1)
+	leaf_page right=newLeafPage_loaded(l,0,size1);
 	l->father->pointer[pivot+1]=right;
 	//left part to insert
-	leaf_page left=newLeafPage_loaded(l,(M+1)/2,size2)
+	leaf_page left=newLeafPage_loaded(l,(M+1)/2,size2);
 	insertPointer_withBuffer(l->father,pivot+1,left);
 	//insert index to pivot+1
 	insertToIndex_withBuffer(l->father,pivot+1,newIdx);
@@ -237,10 +280,46 @@ int splitLeaf(leaf_page l){//related to father
 	return 1;
 
 }
-int splitIndex(index_page index){
+int splitIndex(index_page l){
+	if(l->size<=M)return 0;
+	if(l->father->size==Inf){
+		index_tree tree = (index_tree)l->father;
+		index_page newRoot = newIndexPage();
+		tree->root=newRoot;
+		tree->height++;
+		newRoot->pointer[0]=l;
+		l->father=newRoot;
+	}
+	else if(l->father->size>=M+1)return 0;
+	//safe to load to father
+	int pivot;
+	For(pivot,0,l->father->size)if(l->father->key[pivot]==l->key[0])break;
+	if(pivot==l->father->size)return 0;
+	//determine the new index to insert
+	index_type newIdx=l->key[(M+1)/2];
+	int size1=(M+1)/2;
+	int size2=(M+1-(M+1)/2);
+	//insert pointers to pivot+1 and pivot+2
+	//right part to replace
+	index_page right=newIndexPage_loaded(l,0,size1);
+	l->father->pointer[pivot+1]=right;
+	//left part to insert
+	index_page left=newIndexPage_loaded(l,(M+1)/2+1,size2);
+	insertPointer_withBuffer(l->father,pivot+1,left);
+	//insert index to pivot+1
+	insertToIndex_withBuffer(l->father,pivot+1,newIdx);
+	//construct linkage
+	//change index
+	if(pivot==-1)changeIndex(l->father->father,l->father->key[1],l->father->key[0]);
+	
+	if(l->father->size==M+1&&!splitIndex(l->father))Error(split index error!);
+	//
+	freeLeafPage(l);
+	return 1;
 
 }
 int leftInsertToLeaf(leaf_page l,index_type idx, addr_type addr){
+	Log();
 	if(l->size<M){
 		if(!insertToleaf(l,idx,addr))Error(Unexpected Full leaf!);
 		return 1;
@@ -262,11 +341,12 @@ int leftInsertToLeaf(leaf_page l,index_type idx, addr_type addr){
 }
 
 int rightInsertToLeaf(leaf_page l, index_type idx, addr_type addr){
+	Log();
 	if(l->size<M){
 		if(!insertToleaf(l,idx,addr))Error(Unexpected Full leaf!);
 		return 1;
 	}
-	else if(l->pre==NULL)return 0;
+	else if(l->next==NULL)return 0;
 	else{//idx is smallest
 		index_type lastIdx=l->key[l->size-1];
 		addr_type lastAddr=l->addr[l->size-1];
@@ -277,6 +357,7 @@ int rightInsertToLeaf(leaf_page l, index_type idx, addr_type addr){
 }
 
 int recursiveInsertToLeaf(leaf_page l,index_type idx,addr_type addr){
+	Log();
 	leaf_page cur=l;
 	int isVacant=0;
 	while(cur!=NULL){
@@ -300,38 +381,58 @@ int recursiveInsertToLeaf(leaf_page l,index_type idx,addr_type addr){
 }
 
 int Insert(index_tree T, index_type idx, addr_type addr){
+	Log();
 	if(T->root==NULL)Error(NIL tree!);
 	int i;
 	int treeHeight=T->height;//search depth
 	index_page cur=T->root;//search init
 	For(i,0,treeHeight){
 		//idx in [lo,hi)
-		int lo=findInterval(cur->key,cur->size);
+		int lo=findInterval(cur->key,idx,cur->size);
 		lo++;
 		if(cur->pointer[lo]==NULL&&i<treeHeight-1)Error(NIL index page!);
 		else if(cur->pointer[lo]==NULL)Error(NIL leaf page!);
 		else cur=cur->pointer[lo];
 	}
 	//cur is the leaf
-	if(cur->size<M)insertToleaf(cur,idx,addr);//vacant leaf
+	if(cur->size<M){
+		if(insertToleaf(cur,idx,addr)){//vacant leaf
+			T->l_size++;
+			return 1;
+		}
+	}
 	else if(T->l_size<M*pow(M+1,treeHeight)){//rotate
 		if(recursiveInsertToLeaf(cur,idx,addr)){
 			T->l_size++;
 			return 1;
 		}
-		else return Error(Cannot find the vacant leaf!);
+		else Error(Cannot find the vacant leaf!);
 	}
 	else{//use preset buffer
-		if(insertToleaf_withBuffer(cur,idx,addr))Erro(Fail to split leaf!);
-		if(splitLeaf(cur))Error(Fail to split leaf!);
+		if(!insertToleaf_withBuffer(cur,idx,addr))Error(Fail to split leaf!);
+		else if(!splitLeaf(cur))Error(Fail to split leaf!);
+		else{
+			T->l_size++;
+			return 1;
+		}
 	}
+	return 1;
+}
 
+void putTree(index_tree T){
+	printf("Tree height: %d\nLinked leaves number: %d\n",T->height,T->l_size);
+	return;
 }
 
 int main(void){
 
 	index_tree MainTree=initialIndexTree();
-	int data=7;
-	Insert(MainTree,1,&data);
+	//test sample
+	int sampleSize=100;
+	int data[100]={0};
+	Initial(data,i,100);
+	int i;
+	For(i,0,sampleSize)Insert(MainTree,data[i],data+i);
+	putTree(MainTree);
 	return 0;
 }
