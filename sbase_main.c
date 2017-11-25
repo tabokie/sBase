74b1c02087da2a3ea676c0fa8184f601c179800f
tabokie <james_199@126.com>
2017-11-25 10:31:29 +0800

    Debug tokenizer.

--- sbase_main.c
+++ sbase_main.c
@@ -1,10 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <time.h>
 #include "b_macro.h"
 //#include "bplus.h"
-#include "db_manager.h"
+//#include "db_manager.h"
 
+void putSystemLog(void);
+void printPrompt(void);
+void printUnioned(void);
+/*
+	\ Read input stream and tokenize.
+*/
+typedef struct INPUT_t{
+	char** tokens;
+	int size;
+	int capacity;
+}* inputBuffer;
+#define INIT_BUFFER_s		(20)
+inputBuffer newInputBuffer(void);
+void clearInput(inputBuffer input);
+#define CMD_END_CHR		";"
+void readInput(inputBuffer input);
+#define SPLITER_CHR		" \n"
+void toTokens(inputBuffer input, char* string);
 /*
 	\brief Process meta command and pass db operating command to db manager
 */
@@ -14,11 +33,11 @@
 */
 void putSystemLog(void){
 	// print version
-	printf("sBase 0.0.1 (Nov 24 2017, 11:40) on win32.\n");
+	printf("sBase 0.0.1 | default | (Nov 24 2017, 11:40) on win32.\n");
 
-	// meta command
-	printf("Type .exit to exit prompt interface.\n");
-	printf("Type .help, .copyright, .credits, .license for more information.\n");
+	// meta command guidance
+	printf("Type \".exit;\" to exit prompt interface.\n");
+	printf("Type \".help;\", \".copyright;\", \".credits;\", \".license;\" for more information.\n");
 	return;
 }
 void printPrompt(void){
@@ -28,16 +47,7 @@ void printUnioned(void){
 	printf("... ");
 }
 
-/*
-	\ Read input stream and tokenize.
-*/
-typedef struct INPUT_t{
-	char** tokens;
-	int size;
-	int capacity;
-}* inputBuffer;
 
-#define INIT_BUFFER_s		(20)
 
 inputBuffer newInputBuffer(void){
 	inputBuffer new=New(struct INPUT_t,1);
@@ -55,54 +65,62 @@ void clearInput(inputBuffer input){
 	return;
 }
 
-#define CMD_END_CHR		";"
 void readInput(inputBuffer input){
-	char first_buffer[INIT_BUFFER_s+1]={0};
-	int first_size=INIT_BUFFER_s;
-	char second_buffer[INIT_BUFFER_s+1]={0};
+	char first_buffer;
+	char* second_buffer=New(char, INIT_BUFFER_s+1);
+	Initial(second_buffer, 0, INIT_BUFFER_s+1);
 	int second_size=INIT_BUFFER_s;
-	// if havenot read end
-	while(strlen(first_buffer)==strcspn(first_buffer, CMD_END_CHR)){
-		Initial(first_buffer, 0, first_size);
-		scanf_s("%s",first_buffer,first_size+1);// set_constraint_handler_s ?
-		while(strlen(first_buffer)+strlen(second_buffer)>second_size){
+	int second_end=0;
+	int clause_end=0;
+	//
+	while((first_buffer=getchar())!=-1){
+
+		if(strlen(second_buffer)>=second_size){
 			char* tp=second_buffer;
 			second_buffer=New(char,second_size*2+1);
 			Initial(second_buffer, 0, second_size*2+1);
 			second_size*=2;
-			memcpy(second_buffer, tp, sizeof(char)*strlen(tp));
+			memcpy(second_buffer, tp, sizeof(char)*(strlen(tp)+1));
+			free(tp);
 		}
-		strcat(second_buffer, first_buffer);
-		printUnioned();
+		second_buffer[second_end++]=first_buffer;
+		second_buffer[second_end]='\0';
+		if(strchr(CMD_END_CHR, first_buffer)!=NULL){
+			clause_end=1;
+		}
+		if(clause_end&&first_buffer=='\n')break;
+		else if(first_buffer=='\n')printUnioned();
 	}
-
+	
 	// tokenize
 	toTokens(input, second_buffer);
-	free(first_buffer);
 	free(second_buffer);
-	return input;
+	return;
 }
 
 #define SPLITER_CHR		" \n"
-int toTokens(inputBuffer input, char* string){
-	char* buf;
-	while((buf=strtok(string, SPLITER_CHR))!=NULL){
+void toTokens(inputBuffer input, char* string){
+	char* buf = strtok(string, SPLITER_CHR); // convert spliter to \0
+	if(buf==NULL)return;
+	do{
 		while(input->size>=input->capacity){
 			char** tp=input->tokens;
 			input->tokens=New(char*, input->capacity*2);
 			input->capacity*=2;
 			Initial(input->tokens, NULL, input->capacity);
 			memcpy(input->tokens, tp, sizeof(char*)*input->size);
+			free(tp);
 		}
 		input->tokens[input->size++]=buf;
-	}
+	}while((buf=strtok(NULL, SPLITER_CHR))!=NULL);
+	return;
 }
 
 /*
 	\ Parser: Do classification, and produce pickled paramaters for db manager.
 */
 
-// Now Supported Command:
+// Now Supported Database Command:
 // use {dbname}
 //	-> send message to openDB
 // drop {dbname}
@@ -135,6 +153,7 @@ int main(int argc, char* argv[]){
 	while(true){
 		printPrompt();
 		readInput(input);
+		/*
 		if(input->tokens[0][0]=='.'){
 			metaCommand(input->tokens);
 		}
@@ -145,6 +164,7 @@ int main(int argc, char* argv[]){
 			case(DB_NOT_ACCESSIBLE):Error(DB_NOT_ACCESSIBLE);
 			case(DB_ACCESSIBLE):continue;
 		}
+		*/
 		clearInput(input);
 	}
 	
