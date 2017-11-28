ede6afaa491de4cdde6941ca68d93fae5dac5907
tabokie <james_199@126.com>
2017-11-28 22:21:18 +0800

    Add function: insert routine.

--- db_manager.h
+++ db_manager.h
@@ -7,10 +7,13 @@ typedef struct INDEX_META_t{
 	int row_id;// test
 }* index_meta_t;
 
+#define PAGE_SIZE		(100)
+#define TOP_MAX			(PAGE_SIZE/offset)
 typedef struct PAGER_t{
 	// INID -> page_no and relative addr
 	void* base_addr;
 	int offset;
+	int top;// initial as 0
 }* pager_t;
 
 typedef char* table_name_t;
@@ -32,6 +35,7 @@ typedef struct DATA_TABLE_t{
 	// give INID and row_offset to fetch single data
 	// give insert content and get INID
 	int page_num;
+	int capacity;
 	pager_t* pages;
 
 	// index part
@@ -106,8 +110,30 @@ dropDatabaseResult dropDatabase(void){
 	including: index_trees, entries, I/O methods
 */
 typedef enum{INSERT_TO_TABLE_SUCCESS, INSERT_TO_TABLE_NOT_FOUND, INSERT_TO_TABLE_FAILURE} insertToTableResult; 
-insertToTableResult insertToTable(char* tableName, ){
+insertToTableResult insertToTable(char* tableName, uncertain_type data){
+	char* formatting_data = pickle(data);
+	table_meta_t target_table = getTable(char* tableName);
+	int i,j;
+	for(i=target_table->page_num-1;i>=0;i--){
+		if(target_table->pages[i]->top<TOP_MAX)break;
+	}
+	if(i<0){
+		if(target_table->page_num==target_table->capacity){// preset space full
+			page_t* new=(pager_t*)malloc(sizeof(pager_t)*target_table->capacity*2);
+			for(j=0;j<target_table->capacity;j++){
+				new[j]=target_table->pages[j];
+			}
+			free(target_table->pages);
+			target_table->pages=new;
+			target_table->capacity*=2;
+		}
+		target_table->pages[target_table->page_num++]=newPage(offset);
+		i=target_table->page_num-1;
+	}
 
+	addr_type inid = insertToPage(target_table->pages[i], formatting_data);
+	for row_id: 
+		insertToIndex(,inid)
 }
 
  typedef enum{SELECT_FROM_TABLE_SUCCESS, SELECT_FROM_TABLE_NOT_FOUND, SELECT_FROM_TABLE_FAILURE} selectFromTableResult;
